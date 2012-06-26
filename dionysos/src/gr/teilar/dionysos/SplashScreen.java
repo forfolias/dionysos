f3ff89b7cd41c1fa9fad40a001c9cf6c9ad7f39f
George Vasilakos <forfolias@gmail.com>
2012-06-26 15:16:56 +0300

    Set app name Set splash screen layout Add logo Add icon Add main menu class Update Manifest

--- dionysos/src/gr/teilar/dionysos/SplashScreen.java
+++ dionysos/src/gr/teilar/dionysos/SplashScreen.java
@@ -1,13 +1,31 @@
 package gr.teilar.dionysos;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash_screen);
+
+		ImageView logo = (ImageView) findViewById(R.id.splashScreen);
+		Animation anim = AnimationUtils.loadAnimation(this,
+				R.anim.appear);
+		logo.startAnimation(anim);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainMenu.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, 1200);
     }
 }
