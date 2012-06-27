6e81512c22b1bd88febd68c91c032692710818e2
George Vasilakos <forfolias@gmail.com>
2012-06-27 12:23:23 +0300

    add main menu buttons add preferences screen add about app screen add main menu buttons styles update manifest

--- dionysos/src/gr/teilar/dionysos/MainMenu.java
+++ dionysos/src/gr/teilar/dionysos/MainMenu.java
@@ -1,13 +1,26 @@
 package gr.teilar.dionysos;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainMenu extends Activity {
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_menu);
+
+		findViewById(R.id.preferences_button).setOnClickListener(
+				new OnClickListener() {
+					public void onClick(View v) {
+						Intent i = new Intent(
+								getBaseContext(),
+								gr.teilar.dionysos.PreferencesScreen.class);
+						startActivity(i);
+					}
+				});
+	}
 }
---
+++ dionysos/src/gr/teilar/dionysos/PreferencesScreen.java
@@ -0,0 +1,25 @@
+package gr.teilar.dionysos;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+
+
+public class PreferencesScreen extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences_screen);
+		
+		Preference aboutPref = (Preference) findPreference("aboutPreference");
+		aboutPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				Intent i = new Intent(getBaseContext(),
+						gr.teilar.dionysos.AboutAppScreen.class);
+				startActivity(i);
+				return true;
+			}
+		});
+	}
+}
