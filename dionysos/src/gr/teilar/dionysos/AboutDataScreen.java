dafe06b4e64d379c776a8fbc6df1f2b629ea0703
George Vasilakos <forfolias@gmail.com>
2012-09-19 12:47:04 +0300

    add grades info

--- dionysos/src/gr/teilar/dionysos/AboutDataScreen.java
+++ dionysos/src/gr/teilar/dionysos/AboutDataScreen.java
@@ -14,6 +14,7 @@ import org.xml.sax.SAXException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -23,31 +24,28 @@ public class AboutDataScreen extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about_data_screen);
-		
+
 		findViewById(R.id.update_data_now_info_screen).setOnClickListener(
 				new OnClickListener() {
 					public void onClick(View v) {
-						Intent i = new Intent(
-								getBaseContext(),
+						Intent i = new Intent(getBaseContext(),
 								gr.teilar.dionysos.Dionysos.class);
 						startActivityForResult(i, 1);
 					}
 				});
-		
-//		TextView gradesDate = (TextView) findViewById(R.id.data_info_grades_date_tv);
+
+		TextView gradesDate = (TextView) findViewById(R.id.data_info_grades_date_tv);
 		TextView lessonsDate = (TextView) findViewById(R.id.data_info_lessons_date_tv);
 		TextView requestsDate = (TextView) findViewById(R.id.data_info_requests_date_tv);
-		
-		
-		/* TODO read the xml files and display the correct dates */
-		
-//		gradesDate.setText(getXmlDate("grades"));
+
+		gradesDate.setText(getXmlDate("grades"));
 		lessonsDate.setText(getXmlDate("lessons"));
 		requestsDate.setText(getXmlDate("requests"));
 	}
-	
+
 	private String getXmlDate(String type) {
-		File file = new File("/sdcard/egrammatia/"+type+".xml");
+		File file = new File(Environment.getExternalStorageDirectory()
+				.getPath() + "/egrammatia/" + type + ".xml");
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db;
@@ -56,16 +54,10 @@ public class AboutDataScreen extends Activity {
 			db = dbf.newDocumentBuilder();
 			doc = db.parse(file);
 		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 			return getResources().getString(R.string.not_found);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 			return getResources().getString(R.string.not_found);
 		} catch (ParserConfigurationException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 			return getResources().getString(R.string.not_found);
 		}
 
@@ -73,7 +65,7 @@ public class AboutDataScreen extends Activity {
 		return ((Element) doc.getElementsByTagName(type).item(0))
 				.getAttribute("date");
 	}
-	
+
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == 1) {
 			if (resultCode == RESULT_OK) {
