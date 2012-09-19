774619c635e00a67957950e2e2857e828f3f934d
George Vasilakos <forfolias@gmail.com>
2012-09-19 12:49:32 +0300

    display grades add android-support jar update android build target

--- dionysos/src/gr/teilar/dionysos/GradesScreen.java
+++ dionysos/src/gr/teilar/dionysos/GradesScreen.java
@@ -17,19 +17,27 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class GradesScreen extends Activity {
+	
+	private int eksamina = 7;
+	private int maxEksamina = 7;
+	private LinearLayout[] eksaminaOuterLayout = new LinearLayout[maxEksamina];
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.grades_screen);
-
+		
 		if (Dionysos.checkUpdate(getBaseContext(), "grades")) {
 			Intent i = new Intent(getBaseContext(),
 					gr.teilar.dionysos.Dionysos.class);
@@ -51,11 +59,22 @@ public class GradesScreen extends Activity {
 
 	protected void displayXml() {
 		TextView dataDate = (TextView) findViewById(R.id.grades_data_date);
-		dataDate.setText("TATAN");
-		if(true)
-			return;
+//		dataDate.setText("TATAN");
 		
-		File file = new File("/sdcard/egrammatia/lessons.xml");
+//		for (int j = 0; j < eksamina; j++){
+//			eksaminaTable[j] = new TableLayout(this);
+//			TextView tv = new TextView(cxt);
+//			tv.setText("Bonjour PAUG " + j);
+//			tv.setTextColor(Color.WHITE);
+//			tv.setTextSize(30);
+//			eksaminaTable[j].addView(tv);
+//		}
+		
+//		myPagerAdapter awesomeAdapter = new myPagerAdapter();
+//		ViewPager awesomePager = (ViewPager) findViewById(R.id.awesomepager);
+//		awesomePager.setAdapter(awesomeAdapter);
+				
+		File file = new File(Environment.getExternalStorageDirectory().getPath()+"/egrammatia/grades.xml");
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db;
@@ -75,60 +94,129 @@ public class GradesScreen extends Activity {
 		}
 
 		doc.getDocumentElement().normalize();
-		String date = ((Element) doc.getElementsByTagName("lessons").item(0))
+		String date = ((Element) doc.getElementsByTagName("grades").item(0))
 				.getAttribute("date");
 
-		TextView dataDatea = (TextView) findViewById(R.id.lessons_data_date);
+//		TextView dataDate = (TextView) findViewById(R.id.grades_data_date);
 		dataDate.setText(dataDate.getText() + " " + date);
 		
-		NodeList lessonssList = doc.getElementsByTagName("lesson");
+		NodeList eksaminaList = doc.getElementsByTagName("eksamino");
+		eksamina = eksaminaList.getLength();
 		
-		TableLayout table = (TableLayout) findViewById(R.id.tableLessons);
-		
-		
-		for (int i = 0; i < lessonssList.getLength(); i++){
-			TableRow row = new TableRow(this);
-			LinearLayout layout = new LinearLayout(this);
-			layout.setOrientation(LinearLayout.VERTICAL);
-			View hrLine = new View(this);
-			hrLine.setBackgroundColor(Color.GRAY);
-		
-			if(i%2 == 0)
-				row.setBackgroundColor(getResources().getColor(R.color.row_even));
-			else if(i%2 == 1)
-				row.setBackgroundColor(getResources().getColor(R.color.row_odd));
+		for (int j = 0; j < eksamina; j++){
+			eksaminaOuterLayout[j] = new LinearLayout(this);
+			eksaminaOuterLayout[j].setOrientation(LinearLayout.VERTICAL);
 			
-			TextView titleLine = new TextView(this);
-			TextView subtitleLine = new TextView(this);
-			String eksamino = ((Element) lessonssList.item(i)).getAttribute("eksamino");
-			String dm = ((Element) lessonssList.item(i)).getAttribute("dm");
-			String ores = ((Element) lessonssList.item(i)).getAttribute("ores");
+			ScrollView eksaminaScroll = new ScrollView(this);
+			LinearLayout mathimataLayout = new LinearLayout(this);
+			RelativeLayout mathimaLine = new RelativeLayout(this);
+			TextView eksaminoTitle = new TextView(this);
+			TextView prevnext = null;
 			
-			titleLine.setText(lessonssList.item(i).getChildNodes().item(0).getNodeValue());
-			titleLine.setTypeface(null, Typeface.BOLD);
-			titleLine.setTextSize(14);
+			eksaminoTitle.setText(getResources().getString(R.string.semester)+" "+(j+1)+"o");
+			eksaminoTitle.setTextColor(getResources().getColor(R.color.text_color));
+			eksaminoTitle.setTextSize(15);
+			eksaminoTitle.setTypeface(null, Typeface.BOLD);
 			
-			subtitleLine.setText(
-					getResources().getString(R.string.semester) + ": " + eksamino + " // " +
-					getResources().getString(R.string.hours) + " : " + ores + " // " +
-					getResources().getString(R.string.ects) + " : " + dm
-					);
-			subtitleLine.setTextSize(11);
+			RelativeLayout.LayoutParams align = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			align.addRule(RelativeLayout.CENTER_HORIZONTAL);
+			mathimaLine.addView(eksaminoTitle, align);
+			mathimaLine.setPadding(7, 7, 7, 7);
 			
-			layout.addView(titleLine, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-			layout.addView(subtitleLine, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+			if(j != eksamina-1){
+				prevnext = new TextView(this);
+				prevnext.setText("»»");
+				prevnext.setPadding(0, 0, 10, 0);
+				prevnext.setTypeface(null, Typeface.BOLD);
+				prevnext.setTextSize(15);
+				prevnext.setTextColor(getResources().getColor(R.color.text_color));
+				align = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+				align.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+				mathimaLine.addView(prevnext, align);
+			}
+			if(j != 0){
+				prevnext = new TextView(this);
+				prevnext.setText("««");
+				prevnext.setPadding(10, 0, 0, 0);
+				prevnext.setTypeface(null, Typeface.BOLD);
+				prevnext.setTextSize(15);
+				prevnext.setTextColor(getResources().getColor(R.color.text_color));
+				align = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+				align.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+				mathimaLine.addView(prevnext, align);
+			}
 			
-			 
-			row.addView(layout);
-			row.setPadding(7, 7, 7, 7);
 			
-			table.addView(hrLine, new LayoutParams(LayoutParams.FILL_PARENT, 1));
-			table.addView(row, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+			mathimataLayout.setOrientation(LinearLayout.VERTICAL);
+			eksaminaOuterLayout[j].addView(mathimaLine, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+			
+			NodeList mathimata = ((Element) eksaminaList.item(j)).getElementsByTagName("lesson");
+						
+			for (int i = 0; i < mathimata.getLength(); i++){
+				
+				mathimaLine = new RelativeLayout(this);
+				View hrLine = new View(this);
+				hrLine.setBackgroundColor(Color.GRAY);
+			
+				if(i%2 == 0)
+					mathimaLine.setBackgroundColor(getResources().getColor(R.color.row_even));
+				else if(i%2 == 1)
+					mathimaLine.setBackgroundColor(getResources().getColor(R.color.row_odd));
+				
+				TextView title = new TextView(this);
+				TextView subtitleLine = new TextView(this);
+				TextView vathmos = new TextView(this);
+				String dm = ((Element) mathimata.item(i)).getAttribute("dm");
+				String ores = ((Element) mathimata.item(i)).getAttribute("ores");
+				
+				vathmos.setText(((Element) mathimata.item(i)).getAttribute("vathmos"));
+				vathmos.setTypeface(null, Typeface.BOLD);
+				vathmos.setTextSize(15);
+				vathmos.setTextColor(getResources().getColor(R.color.text_color));
+				
+				title.setText(mathimata.item(i).getChildNodes().item(0).getNodeValue());
+				title.setTypeface(null, Typeface.BOLD);
+				title.setTextSize(14);
+				title.setTextColor(getResources().getColor(R.color.text_color));
+				title.setId((10*(j+1))+i);
+				
+				subtitleLine.setText(
+						getResources().getString(R.string.hours) + " : " + ores + " // " +
+						getResources().getString(R.string.ects) + " : " + dm
+						);
+				subtitleLine.setTextSize(11);
+				subtitleLine.setTextColor(getResources().getColor(R.color.text_color));
+				
+				align = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+				align.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+				align.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+				mathimaLine.addView(title, align);
+				
+				align = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+				align.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+				align.addRule(RelativeLayout.BELOW, title.getId());
+				mathimaLine.addView(subtitleLine, align);
+				
+				align = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+				align.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+				align.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+				mathimaLine.addView(vathmos, align);
+				
+				mathimaLine.setPadding(7, 7, 7, 7);
+				
+				mathimataLayout.addView(mathimaLine, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+			}
+			eksaminaScroll.addView(mathimataLayout);
+			eksaminaOuterLayout[j].addView(eksaminaScroll);
 		}
+		
+		myPagerAdapter awesomeAdapter = new myPagerAdapter();
+		ViewPager awesomePager = (ViewPager) findViewById(R.id.eksaminapager);
+		awesomePager.setAdapter(awesomeAdapter);
 	}
 	
 	private void displayError() {
-		((TextView) findViewById(R.id.lessons_data_date)).setText(getResources().getString(R.string.file_error));
+		((TextView) findViewById(R.id.grades_data_date)).setText(getResources().getString(R.string.file_error));
 	
 	}
 
@@ -145,4 +233,42 @@ public class GradesScreen extends Activity {
 			}
 		}
 	}
+	
+
+    private class myPagerAdapter extends PagerAdapter{
+		@Override
+		public int getCount() {
+			return eksamina;
+		}
+
+		@Override
+		public Object instantiateItem(View collection, int position) {
+			((ViewPager) collection).addView(eksaminaOuterLayout[position], 0);
+			return eksaminaOuterLayout[position];
+		}
+
+		@Override
+		public void destroyItem(View collection, int position, Object view) {
+			((ViewPager) collection).removeView((LinearLayout) view);
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			return view==((LinearLayout)object);
+		}
+
+		@Override
+		public void finishUpdate(View arg0) {}
+
+		@Override
+		public void restoreState(Parcelable arg0, ClassLoader arg1) {}
+
+		@Override
+		public Parcelable saveState() {
+			return null;
+		}
+
+		@Override
+		public void startUpdate(View arg0) {}
+    }
 }
