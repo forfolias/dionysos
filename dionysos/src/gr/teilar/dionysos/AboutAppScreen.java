bd813f94ea34b0bf1d0ead8007c6cec0fb4143c1
George Vasilakos <forfolias@gmail.com>
2012-09-20 11:23:48 +0300

    fix version string

--- dionysos/src/gr/teilar/dionysos/AboutAppScreen.java
+++ dionysos/src/gr/teilar/dionysos/AboutAppScreen.java
@@ -1,8 +1,11 @@
 package gr.teilar.dionysos;
 
 import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 public class AboutAppScreen extends Activity {
 	@Override
@@ -10,5 +13,19 @@ public class AboutAppScreen extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.about_app_screen);
+		
+		try {
+			((TextView) findViewById(R.id.version))
+			.setText(
+					getResources().getString(R.string.version) + 
+					getPackageManager().getPackageInfo(getPackageName(), 0).versionName
+			);
+		} catch (NotFoundException e) {
+			((TextView) findViewById(R.id.version))
+			.setText(getResources().getString(R.string.version)+" 1.0");
+		} catch (NameNotFoundException e) {
+			((TextView) findViewById(R.id.version))
+			.setText(getResources().getString(R.string.version)+" 1.0");
+		}
 	}
 }
